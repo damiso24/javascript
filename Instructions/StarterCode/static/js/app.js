f307a2dd088f0b2680e4da769a57439f3f91fb91
Damiso Hutchinson <d.hutchinson24@hotmail.com>
2021-06-30 19:04:15 -0400

    final

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,35 +1,59 @@
 // from data.js
 var sightings = data;
-
-// YOUR CODE HERE!
 console.log(sightings);
+
+// select tobdy from html
+var tbody = d3.select("tbody");
+
+sightings.forEach(sighting=>{
+  // console.log(sighting);
+  var row = tbody.append("tr");
+  
+  Object.entries(sighting).forEach(([key, value])=>{
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+});
 
 
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // Select the form
 var form = d3.select("#form");
 
-// Create event handlers 
+// Event handlers 
 button.on("click", runFilter);
 form.on("submit",runFilter);
 
-// Complete the event handler function for the form
+// Filter funcion
 function runFilter() {
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
   
-  // Select the input element and get the raw HTML node
+  // Select the input element
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
-
   console.log(inputValue);
-  console.log(sightings);
 
-  var filteredData = sightings.filter(date => date.datetime === inputValue);
+  // Use inputValue to filter data.
+  var filteredData = sightings.filter(sighting => sighting.datetime === inputValue);
 
-  console.log(filteredData);}
+  // Clear the table
+  tbody.html("")
+
+  // Loop through filter and add a row for each row of data
+  filteredData.forEach(filteredData=>{
+    console.log(filteredData);
+    var row = tbody.append("tr");
+    
+    // fill the rows cell by cell with each key, value pair of filtered data
+    Object.entries(filteredData).forEach(([key, value])=>{
+        var cell = tbody.append("td");
+        cell.text(value);
+      });
+  });
+}
